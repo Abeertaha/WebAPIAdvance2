8ee1df6706de4444183da121754b9ed955d71741
Abeer Taha <104815752+Abeertaha@users.noreply.github.com>
2023-07-01 09:23:45 +0300

    Create EmployeeVM.cs

---
+++ ViewModels/EmployeeVM.cs
@@ -0,0 +1,11 @@
+
+namespace EmployeeApp.ViewModels
+{
+    public class EmployeeVM
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+    }
+}
